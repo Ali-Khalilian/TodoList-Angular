38403641b8250160b9b6406fd74a6a0031801b00
Ali Khalilian <alikhaliliankh12@gmail.com>
2025-10-01 20:44:01 +0330

    Initial commit: Add TodoList project (API + Angular client)

---
+++ client/src/app/app.component.ts
@@ -0,0 +1,28 @@
+import { HttpClient } from '@angular/common/http';
+import { Component, inject, OnInit } from '@angular/core';
+import { RouterOutlet } from '@angular/router';
+
+@Component({
+  selector: 'app-root',
+  standalone: true,
+  imports: [RouterOutlet],
+  templateUrl: './app.component.html',
+  styleUrl: './app.component.css'
+})
+export class AppComponent implements OnInit {
+  http = inject(HttpClient);
+  title = 'client';
+  users: any;
+
+  ngOnInit(): void {
+    this.http.get('https://localhost:5011/api/users').subscribe({
+      next: response => this.users = response,
+      error: (e) => {
+        console.log(e);
+      },
+      complete: () => {
+        console.log('Request has completed');
+      }
+    });
+  }
+}
